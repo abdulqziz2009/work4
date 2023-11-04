30bf2c9b981f70370f2f10c5b68520a6456c8ad4
abdulqziz2009 <148250194+abdulqziz2009@users.noreply.github.com>
2023-11-04 13:22:35 +0600

    Update Main.java

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,73 @@
+class Hero {
+    private int health;
+    private int damage;
+    private String superpower;
+    public Hero(int health, int damage, String superpower) {
+        this.health = health;
+        this.damage = damage;
+        this.superpower = superpower;
+    }
+
+    public Hero(int health, int damage) {
+        this.health = health;
+        this.damage = damage;
+        this.superpower = "Нет суперспособности";
+    }
+
+    public int getHealth() {
+        return health;
+    }
+
+    public int getDamage() {
+        return damage;
+    }
+
+    public String getSuperpower() {
+        return superpower;
+    }
+}
+
+class Boss {
+    private int health;
+    private int damage;
+    private String defenseType;
+
+    public int getHealth() {
+        return health;
+    }
+
+    public void setHealth(int health) {
+        this.health = health;
+    }
+
+    public int getDamage() {
+        return damage;
+    }
+
+    public void setDamage(int damage) {
+        this.damage = damage;
+    }
+
+    public String getDefenseType() {
+        return defenseType;
+    }
+
+    public void setDefenseType(String defenseType) {
+        this.defenseType = defenseType;
+    }
+}
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        Boss boss = new Boss();
+
+        boss.setHealth(1000);
+        boss.setDamage(200);
+        boss.setDefenseType("Броня");
+
+        System.out.println("Информация о боссе:");
+        System.out.println("Здоровье: " + boss.getHealth());
+        System.out.println("Урон: " + boss.getDamage());
+        System.out.println("Тип защиты: " + boss.getDefenseType());
     }
 }
